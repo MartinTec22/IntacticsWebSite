f325b874e7a60a81d8d89c11509ca857b65dd369
MartinTec22 <17340352@itnogales.edu.mx>
2021-10-27 09:59:18 -0700

    Se agrega proyecto

---
+++ js/carousel.js
@@ -0,0 +1,135 @@
+(function ($) {
+	"use strict";
+
+	// meanmenu
+	$('#mobile-menu').meanmenu({
+		meanMenuContainer: '.mobile-menu',
+		meanScreenWidth: "992"
+	});
+
+	$('.info-bar').on('click', function () {
+		$('.extra-info').addClass('info-open');
+	})
+
+	$('.close-icon').on('click', function () {
+		$('.extra-info').removeClass('info-open');
+	})
+
+	// sticky
+	var wind = $(window);
+	var sticky = $('#sticky-header');
+	wind.on('scroll', function () {
+		var scroll = wind.scrollTop();
+		if (scroll < 100) {
+			sticky.removeClass('sticky');
+		} else {
+			sticky.addClass('sticky');
+		}
+	});
+
+
+	// mainSlider
+	function mainSlider() {
+		var BasicSlider = $('.slider-active');
+		BasicSlider.on('init', function (e, slick) {
+			var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+			doAnimations($firstAnimatingElements);
+		});
+		BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+			var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+			doAnimations($animatingElements);
+		});
+		BasicSlider.slick({
+			autoplay: false,
+			autoplaySpeed: 10000,
+			dots: false,
+			fade: true,
+			arrows: true,
+			prevArrow: '<button type="button" class="slick-prev"><i class="far fa-angle-double-left"></i></button>',
+			nextArrow: '<button type="button" class="slick-next"><i class="far fa-angle-double-right"></i></button>',
+			responsive: [
+				{
+					breakpoint: 1200,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						infinite: true,
+					}
+				},
+				{
+					breakpoint: 991,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						arrows: false,
+					}
+				},
+				{
+					breakpoint: 767,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						arrows: false,
+					}
+				}
+			]
+		});
+
+		function doAnimations(elements) {
+			var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+			elements.each(function () {
+				var $this = $(this);
+				var $animationDelay = $this.data('delay');
+				var $animationType = 'animated ' + $this.data('animation');
+				$this.css({
+					'animation-delay': $animationDelay,
+					'-webkit-animation-delay': $animationDelay
+				});
+				$this.addClass($animationType).one(animationEndEvents, function () {
+					$this.removeClass($animationType);
+				});
+			});
+		}
+	}
+	mainSlider();
+
+	// services-02-active
+	$('.services-02-active').slick({
+		dots: true,
+		arrows: true,
+		infinite: true,
+		autoplay: true,
+		speed: 300,
+		prevArrow: '<button type="button" class="slick-prev"><i class="far fa-angle-double-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="far fa-angle-double-right"></i></button>',
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true,
+				}
+			},
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					arrows: false,
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+				}
+			}
+		]
+	});
+
+})(jQuery);
